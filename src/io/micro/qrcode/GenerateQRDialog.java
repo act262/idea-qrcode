ba12d72da0ce7439a5f1d5193c1ec17e2be98b3a
act262 <act262@gmail.com>
2017-09-14 15:04:11 +0800

    Update....

--- src/io/micro/qrcode/GenerateQRDialog.java
+++ src/io/micro/qrcode/GenerateQRDialog.java
@@ -4,29 +4,46 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.util.text.StringUtil;
 
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 
 /**
+ * 生成二维码的窗口
+ *
  * @author act262@gmail.com
  */
 public class GenerateQRDialog extends JDialog {
     private JPanel contentPane;
+    private JEditorPane editorPane1;
     private JButton buttonOK;
-    private JTextArea textArea1;
+    private JPanel imagePanel;
+    private QRImageCanvas canvas;
 
-    public GenerateQRDialog() {
+    GenerateQRDialog() {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
+        editorPane1.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    render();
+                }
             }
         });
+        buttonOK.addActionListener(e -> onOK());
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -37,11 +54,11 @@ public class GenerateQRDialog extends JDialog {
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        contentPane.registerKeyboardAction(e ->
+                onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        canvas = new QRImageCanvas();
+        imagePanel.add(canvas);
     }
 
     private void onOK() {
@@ -55,21 +72,30 @@ public class GenerateQRDialog extends JDialog {
     }
 
     private void render() {
-        String contents = textArea1.getText();
-        JFrame f = new JFrame();
-        QRImageCanvas mc = new QRImageCanvas();
-        f.add(mc);
+        String contents = editorPane1.getText();
+        if (StringUtil.isEmptyOrSpaces(contents)) {
+            showNotify("没有内容");
+            return;
+        }
+
         try {
-            BitMatrix bitMatrix = new MultiFormatWriter().encode(contents, BarcodeFormat.QR_CODE, 200, 200);
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(contents, BarcodeFormat.QR_CODE, 100, 100);
             BufferedImage bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
-            mc.setImage(bufferedImage);
+            canvas.setImage(bufferedImage);
+            canvas.repaint();
         } catch (Exception e) {
             e.printStackTrace();
+            showNotify("error");
         }
-        mc.repaint();
-        f.setSize(300, 300);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
+    }
+
+    private void showNotify(String message) {
+        ApplicationManager.getApplication().invokeLater(() -> {
+            Notification notification =
+                    NotificationGroup.balloonGroup("tip")
+                            .createNotification("tip", message, NotificationType.INFORMATION, null);
+            Notifications.Bus.notify(notification);
+        });
     }
 
     public static void main(String[] args) {
@@ -78,4 +104,5 @@ public class GenerateQRDialog extends JDialog {
         dialog.setVisible(true);
         System.exit(0);
     }
+
 }
